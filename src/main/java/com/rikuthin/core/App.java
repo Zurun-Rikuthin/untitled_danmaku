3be248cc0222d9f331f2eba225dc274b0bdc4e5d
Rikuthin <125291285+Zurun-Rikuthin@users.noreply.github.com>
2025-03-07 11:59:16 -0400

    Restructured files

---
+++ src/main/java/com/rikuthin/core/App.java
@@ -0,0 +1,25 @@
+package com.rikuthin.core;
+
+import javax.swing.SwingUtilities;
+
+import com.rikuthin.graphics.GameFrame;
+
+public class App {
+
+    /**
+     * How often the various threads should update themselves (milliseconds).
+     */
+    public static final int TICK_SPEED_MS = 10;
+
+    /**
+     * The entry point for the application. This method schedules the creation
+     * of the {@link GameFrame} on the Event Dispatch Thread (EDT).
+     *
+     * @param args The command line arguments, though not used in this
+     * application.
+     */
+    public static void main(String[] args) {
+        // Schedules GameFrame creation on the EDT
+        SwingUtilities.invokeLater(GameFrame::new);
+    }
+}
---
+++ src/main/java/com/rikuthin/effects/ImageFX.java
@@ -0,0 +1,10 @@
+package com.rikuthin.effects;
+
+import java.awt.Graphics2D;
+
+public interface ImageFX {
+
+    public void update();
+
+    public void draw(Graphics2D g2d);
+}
